c03eadaecc19eb318bf873bdf263b8c5be995795
Alex Suraci <i.am@toogeneric.com>
2009-11-16 22:35:00 -0500

    * Shortened paste IDs to 5 characters. * Added newName(), which will repeatedly generate an ID until it finds   one not being used. (Chances of this being needed are small, but, what   the hell.) * Short URLs for paste viewing: http://url/f6aAf * Generate the seed in main(). * Moved main() to the bottom and gofmt'd the source;

--- paste.go
+++ paste.go
@@ -6,6 +6,7 @@ import (
 	"path";
 	"http";
 	"io";
+	"os";
 	"log";
 	"rand";
 	"strings";
@@ -24,7 +25,7 @@ var addr = flag.String("addr", ":8000", "http service address")
 var fmap = template.FormatterMap{
 	"html": template.HTMLFormatter,
 	"url+html": UrlHtmlFormatter,
-	"pretty": CodePrinter
+	"pretty": CodePrinter,
 }
 
 var homeStr = "<!DOCTYPE html>" + Html(
@@ -35,19 +36,18 @@ var homeStr = "<!DOCTYPE html>" + Html(
 			"href": "/css",
 			"type": "text/css",
 			"media": "screen",
-			"charset": "utf-8"
+			"charset": "utf-8",
 		}),
 		Script("").Attrs(As{
 			"src": "/jquery",
 			"type": "text/javascript",
-			"charset": "utf-8"
+			"charset": "utf-8",
 		}),
 		Script("").Attrs(As{
 			"src": "/js",
 			"type": "text/javascript",
-			"charset": "utf-8"
-		})
-	),
+			"charset": "utf-8",
+		})),
 	Body(
 		Div(
 			Form(
@@ -56,34 +56,33 @@ var homeStr = "<!DOCTYPE html>" + Html(
 						Textarea("").Attrs(As{
 							"class": "paste-input",
 							"rows": "30",
-							"name": "code"
+							"name": "code",
 						}),
 						Ul(
 							Li("Tab key inserts tabstops."),
-							Li("Mod+S to submit.")//,
+							Li("Mod+S to submit.")	//,
 							/*Li(*/
-								/*Input().Attrs(As{*/
-									/*"type": "checkbox",*/
-									/*"class": "paste-private",*/
-									/*"name": "private"*/
-								/*}),*/
-								/*"Private"*/
+							/*Input().Attrs(As{*/
+							/*"type": "checkbox",*/
+							/*"class": "paste-private",*/
+							/*"name": "private"*/
+							/*}),*/
+							/*"Private"*/
 							/*)*/
 						).Attrs(As{
-							"class": "paste-notes"
-						})
-					),
+							"class": "paste-notes",
+						})),
 					Input().Attrs(As{
 						"class": "paste-submit",
 						"type": "submit",
 						"value": "Go Paste!",
-						"accesskey": "s"
+						"accesskey": "s",
 					}))).Attrs(As{
-					"action": "/add",
-					"method": "POST",
-				})).Attrs(As{
-			"id": "home"
-			}))).Out()
+				"action": "/add",
+				"method": "POST",
+			})).Attrs(As{
+			"id": "home",
+		}))).Out()
 var homeTempl = template.MustParse(homeStr, fmap)
 
 var viewStr = "<!DOCTYPE html>" + Html(
@@ -94,82 +93,63 @@ var viewStr = "<!DOCTYPE html>" + Html(
 			"href": "css",
 			"type": "text/css",
 			"media": "screen",
-			"charset": "utf-8"
-		})
-	),
+			"charset": "utf-8",
+		})),
 	Body(
 		Div(
 			A("raw").Attrs(As{
 				"href": "/raw?paste={@|url+html}",
-				"class": "raw"
+				"class": "raw",
 			}),
-			"{@|pretty}"
-		).Attrs(As{
-			"id": "view"
+			"{@|pretty}").Attrs(As{
+			"id": "view",
 		}))).Out()
 var viewTempl = template.MustParse(viewStr, fmap)
 
-func main() {
-	flag.Parse();
-	http.Handle("/", http.HandlerFunc(home));
-	http.Handle("/add", http.HandlerFunc(add));
-	http.Handle("/raw", http.HandlerFunc(raw));
-	http.Handle("/view", http.HandlerFunc(view));
-	http.Handle("/css", http.HandlerFunc(css));
-	http.Handle("/jquery", http.HandlerFunc(jquery));
-	http.Handle("/js", http.HandlerFunc(js));
+func css(c *http.Conn, req *http.Request)	{ http.ServeFile(c, req, "paste.css") }
 
-	err := http.ListenAndServe(*addr, nil);
-	if err != nil {
-		log.Exit("ListenAndServe:", err)
-	}
-}
+func jquery(c *http.Conn, req *http.Request)	{ http.ServeFile(c, req, "jquery.js") }
 
-func css(c *http.Conn, req *http.Request) {
-	http.ServeFile(c, req, "paste.css");
-}
+func js(c *http.Conn, req *http.Request)	{ http.ServeFile(c, req, "paste.js") }
 
-func jquery(c *http.Conn, req *http.Request) {
-	http.ServeFile(c, req, "jquery.js");
-}
+func home(c *http.Conn, req *http.Request) {
+	_, path := path.Split(req.URL.Path);
 
-func js(c *http.Conn, req *http.Request) {
-	http.ServeFile(c, req, "paste.js");
-}
-
-func home(c *http.Conn, req *http.Request)	{
-	if req.Method == "POST" && len(strings.TrimSpace(req.FormValue("code"))) > 0 {
+	switch {
+	case req.Method == "POST" && len(strings.TrimSpace(req.FormValue("code"))) > 0:
 		paste := savePaste(req.FormValue("code"));
 		c.SetHeader("Content-type", "text/plain; charset=utf-8");
-		c.Write(strings.Bytes("http://" + DOMAIN + "/view?paste=" + paste + "\n"));
-	} else {
+		c.Write(strings.Bytes("http://" + DOMAIN + "/" + paste + "\n"));
+	case len(path) > 0:
+		viewTempl.Execute(path, c);
+	default:
 		homeTempl.Execute(nil, c)
 	}
 }
 
-func add(c *http.Conn, req *http.Request)	{
+func add(c *http.Conn, req *http.Request) {
 	if req.Method == "POST" && len(strings.TrimSpace(req.FormValue("code"))) > 0 {
 		paste := savePaste(req.FormValue("code"));
-		c.SetHeader("Location", "/view?paste=" + paste);
+		c.SetHeader("Location", "/"+paste);
 		c.WriteHeader(http.StatusFound);
 	} else {
-		c.Write(strings.Bytes("No code submitted.\n"));
+		c.Write(strings.Bytes("No code submitted.\n"))
 	}
 }
 
 func raw(c *http.Conn, req *http.Request) {
 	if len(req.FormValue("paste")) > 0 {
-		http.ServeFile(c, req, "pastes" + path.Clean("/" + req.FormValue("paste")));
+		http.ServeFile(c, req, "pastes"+path.Clean("/"+req.FormValue("paste")))
 	} else {
-		c.Write(strings.Bytes("No paste specified.\n"));
+		c.Write(strings.Bytes("No paste specified.\n"))
 	}
 }
 
-func view(c *http.Conn, req *http.Request)	{
+func view(c *http.Conn, req *http.Request) {
 	if len(req.FormValue("paste")) > 0 {
-		viewTempl.Execute(req.FormValue("paste"), c);
+		viewTempl.Execute(req.FormValue("paste"), c)
 	} else {
-		c.Write(strings.Bytes("No paste specified.\n"));
+		c.Write(strings.Bytes("No paste specified.\n"))
 	}
 }
 
@@ -177,8 +157,8 @@ func UrlHtmlFormatter(w io.Writer, v interface{}, _ string) {
 	template.HTMLEscape(w, strings.Bytes(http.URLEscape(v.(string))))
 }
 
-func CodePrinter(w io.Writer, v interface {}, _ string) {
-	source, ok := io.ReadFile("pastes" + path.Clean("/" + v.(string)));
+func CodePrinter(w io.Writer, v interface{}, _ string) {
+	source, ok := io.ReadFile("pastes" + path.Clean("/"+v.(string)));
 
 	if ok != nil {
 		fmt.Fprintf(w, "Could not read paste:\n\t%s\n", ok);
@@ -197,17 +177,14 @@ func CodePrinter(w io.Writer, v interface {}, _ string) {
 				A("%d").Attrs(As{
 					"rel": "#L%d",
 					"href": "#LC%d",
-					"id": "LID%d"
-				}).Out() + "\n",
-				line, line, line, line
-			)
-		);
+					"id": "LID%d",
+				}).Out()+"\n",
+				line, line, line, line));
 		codePre.Append(
 			Div(code).Attrs(As{
 				"class": "line",
-				"id": "LC" + fmt.Sprint(line)
-			}).Out()
-		);
+				"id": "LC" + fmt.Sprint(line),
+			}).Out());
 	}
 	fmt.Fprint(
 		w,
@@ -215,36 +192,43 @@ func CodePrinter(w io.Writer, v interface {}, _ string) {
 			Tbody(
 				Tr(
 					Td(linesPre).Attrs(As{"width": "1%", "valign": "top"}),
-					Td(codePre).Attrs(As{"valign": "top"})
-				)
-			)
-		).Attrs(As{
+					Td(codePre).Attrs(As{"valign": "top"})))).Attrs(As{
 			"class": "code",
 			"cellspacing": "0",
-			"cellpadding": "0"
-		}).Out()
-	);
+			"cellpadding": "0",
+		}).Out());
 }
 
 func savePaste(source string) string {
-	paste := randomString(32);
-	io.WriteFile("pastes/" + paste, strings.Bytes(source), 0644);
+	paste := newName();
+	io.WriteFile("pastes/"+paste, strings.Bytes(source), 0644);
 	return paste;
 }
 
-func randomString(length int) string {
-	rand.Seed(time.Nanoseconds());
+func newName() (name string) {
+	name = randomString(5);
+	_, err := os.Open("pastes/" + name, os.O_RDONLY, 0);
 
-	var rng, offset int;
+	if err != nil {
+		return;
+	}
+
+	return newName();
+}
+
+func randomString(length int) string {
+	var rng, offset, mode int;
 
 	str := make([]int, length);
 	for i := 0; i < length; i++ {
-		if i % 3 == 0 {
-			rng, offset = 26, 65;
-		} else if i % 2 == 0 {
-			rng, offset = 26, 97;
+		mode = rand.Intn(3);
+
+		if mode == 0 {
+			rng, offset = 26, 65
+		} else if mode == 1 {
+			rng, offset = 26, 97
 		} else {
-			rng, offset = 10, 48;
+			rng, offset = 10, 48
 		}
 
 		str[i] = rand.Intn(rng) + offset;
@@ -253,3 +237,21 @@ func randomString(length int) string {
 	return string(str);
 }
 
+func main() {
+	rand.Seed(time.Nanoseconds());
+
+	flag.Parse();
+
+	http.Handle("/", http.HandlerFunc(home));
+	http.Handle("/add", http.HandlerFunc(add));
+	http.Handle("/raw", http.HandlerFunc(raw));
+	http.Handle("/view", http.HandlerFunc(view));
+	http.Handle("/css", http.HandlerFunc(css));
+	http.Handle("/jquery", http.HandlerFunc(jquery));
+	http.Handle("/js", http.HandlerFunc(js));
+
+	err := http.ListenAndServe(*addr, nil);
+	if err != nil {
+		log.Exit("ListenAndServe:", err)
+	}
+}
